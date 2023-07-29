3a129b8a36cd9ddc6ac37a4e06b6fc16e7d01f09
Ahmed <aemad430@gmail.com>
2023-07-29 14:54:13 +0300

    all files

--- shell_commands.sh
+++ shell_commands.sh
@@ -5,7 +5,7 @@ mkdir ~/vagrant-shared/block_name
 
 grep -E "block_name" run.log
 
-grep -E "module instance_name block_name parameters" run.log 
+grep -f "module instance_name block_name parameters" run.log 
 
 grep -E "module instance_name block_name inputs" run.log | sed 's/.* first_input_name/first_input_name/' > ~/vagrant-shared/block_name/inputs.log
 
