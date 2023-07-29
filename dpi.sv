0ab5b08d5abdd7457aeb054ad007d7d7806b711e
Ahmed <aemad430@gmail.com>
2023-07-29 14:51:58 +0300

    all files

--- dpi.sv
+++ dpi.sv
@@ -1,13 +1,15 @@
+`ifdef DBG_TRACE_CORE_ICACHE
+always @(posedge clk) begin
+        
+    dpi_trace ("module %m block_name parameters width= %0d, length= %0d\n", width, length);
 
+    dpi_trace ("module %m block_name inputs reset= %0d, valid= %0d\n", reset, valid);
 
-dpi_trace ("module %m block_name parameters width= %0d, length= %0d\n", width, length);
-
-dpi_trace ("module %m block_name inputs reset= %0d, valid= %0d\n", reset, valid);
-
-dpi_trace ("module %m block_name outputs valid= %0d", valid);
-dpi_trace ("result=");
-for(int i=0;i<ARR_SIZE;i++){
-    dpi_trace(" %0d,", result[i]);
-}
-dpi_trace("\n");
-
+    dpi_trace ("module %m block_name outputs valid= %0d", valid);
+    dpi_trace ("result=");
+    for(int i=0;i<ARR_SIZE;i++){
+        dpi_trace(" %0d,", result[i]);
+    }
+    dpi_trace("\n");
+end
+`endif
--- shell_commands.sh
+++ shell_commands.sh
@@ -1,3 +1,6 @@
+make -C driver clean
+CONFIGS="-DDNUM_BANKS=1" ./ci/blackbox.sh --driver=rtlsim --cores=1 --app=io_addr --debug
+
 mkdir ~/vagrant-shared/block_name
 
 grep -E "block_name" run.log
